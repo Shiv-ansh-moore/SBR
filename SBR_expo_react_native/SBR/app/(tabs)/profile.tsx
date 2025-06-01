acaf2da13d1f8d27a42e5ef6bb726c95cac62916
Shivansh Moore <shivansh.moore57@mail.dcu.ie>
2025-06-01 12:07:22 +0100

    camera component made can flip and close camera

--- SBR_expo_react_native/SBR/app/(tabs)/profile.tsx
+++ SBR_expo_react_native/SBR/app/(tabs)/profile.tsx
@@ -1,20 +1,30 @@
-import { View, Text, Button } from "react-native";
+import Camera from "@/components/camera/Camera";
+import React, { useState } from "react";
+import { Button, View } from "react-native";
 import { supabase } from "../../lib/supabaseClient";
 
 const profile = () => {
-  return (
-    <View>
-      <Button
-        title="Sign Out"
-        onPress={async () => {
-          try {
-            await supabase.auth.signOut();
-          } catch (error) {
-            console.error("Error signing out:", error);
-          }
-        }}
-      />
-    </View>
-  );
+  const [showCamera, setShowCamera] = useState(false);
+
+  if (showCamera) {
+    return <Camera setShowCamera = {setShowCamera} />;
+  }
+  if (!showCamera) {
+    return (
+      <View>
+        <Button title="Open Camera" onPress={() => setShowCamera(true)} />
+        <Button
+          title="Sign Out"
+          onPress={async () => {
+            try {
+              await supabase.auth.signOut();
+            } catch (error) {
+              console.error("Error signing out:", error);
+            }
+          }}
+        />
+      </View>
+    );
+  }
 };
 export default profile;
