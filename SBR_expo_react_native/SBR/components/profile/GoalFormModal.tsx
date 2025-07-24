c80bb14173f8e1a57f761332c4eb9731cf447754
Shivansh Moore <shivansh.moore57@mail.dcu.ie>
2025-07-24 13:56:40 +0100

    Modal for add goals done

--- SBR_expo_react_native/SBR/components/profile/GoalFormModal.tsx
+++ SBR_expo_react_native/SBR/components/profile/GoalFormModal.tsx
@@ -1,24 +1,63 @@
-import { Dispatch, SetStateAction } from "react";
-import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Dispatch, SetStateAction, useState } from "react";
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 interface GoalFormModalProps {
   setShowAddGoal: Dispatch<SetStateAction<boolean>>;
   showAddGoal: boolean;
 }
-const GoalFormModal = ({
-  setShowAddGoal,
-  showAddGoal,
-}: GoalFormModalProps) => {
+const GoalFormModal = ({ setShowAddGoal, showAddGoal }: GoalFormModalProps) => {
+  const [showDueDatePicker, setShowDueDatePicker] = useState<boolean>(false);
+
   return (
     <View>
       <Modal transparent={true} visible={showAddGoal} animationType="fade">
         {/* This wrapper View centers the content */}
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text>GoalFormModal</Text>
-            <TouchableOpacity onPress={() => setShowAddGoal(false)}>
-              <Text>Close</Text>
-            </TouchableOpacity>
+            <Text style={styles.heading}>Add Goal:</Text>
+            <Text style={styles.title}>Title:</Text>
+            <TextInput
+              style={styles.titleInput}
+              autoCapitalize="words"
+              autoFocus={true}
+            ></TextInput>
+            <Text style={styles.title}>Description:</Text>
+            <TextInput
+              style={styles.descriptionInput}
+              autoCapitalize="words"
+              multiline={true}
+            ></TextInput>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => setShowDueDatePicker(true)}
+                style={[styles.buttons, styles.dueDateButton]}
+              >
+                <Ionicons
+                  name="add-circle"
+                  size={18}
+                  color="#3ECF8E"
+                  style={{ marginRight: 3 }}
+                />
+                <Text style={[styles.buttonText]}>Due Date</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.buttons, styles.closeButton]}
+                onPress={() => setShowAddGoal(false)}
+              >
+                <Text style={[styles.buttonText]}>Close</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.buttons, styles.addButton]}>
+                <Text style={[styles.buttonText]}>Add Goal</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -36,11 +75,74 @@ const styles = StyleSheet.create({
   },
 
   modalView: {
-    height: 333,
+    height: 290,
     width: "95%",
     backgroundColor: "#171717",
     borderRadius: 20,
     borderColor: "rgba(77, 61, 61, 0.50)",
     borderWidth: 1,
   },
+  heading: {
+    fontSize: 24,
+    fontFamily: "SemiBold",
+    color: "white",
+    marginLeft: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: "Regular",
+    color: "white",
+    marginLeft: 5,
+  },
+  titleInput: {
+    backgroundColor: "#242424",
+    borderWidth: 1,
+    borderColor: "rgba(77, 61, 61, 0.50)",
+    borderRadius: 20,
+    color: "white",
+    fontFamily: "Light",
+    textAlignVertical: "top",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  descriptionInput: {
+    backgroundColor: "#242424",
+    borderWidth: 1,
+    borderColor: "rgba(77, 61, 61, 0.50)",
+    borderRadius: 20,
+    color: "white",
+    fontFamily: "Light",
+    height: 100,
+    textAlignVertical: "top",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: "space-between",
+  },
+  buttons: {
+    height: 30,
+    width: 110,
+    borderRadius: 15,
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontFamily: "Regular",
+    textAlign: "center",
+  },
+  dueDateButton: {
+    flexDirection: "row",
+    backgroundColor: "#242424",
+    borderWidth: 1,
+    borderColor: "rgba(77, 61, 61, 0.50)",
+    alignItems: "center",
+  },
+  closeButton: { backgroundColor: "red" },
+  addButton: { backgroundColor: "#3ECF8E" },
 });
